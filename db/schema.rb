546c29601393754f4d842eb2a341b0d424ebf14f
bressani <jr.bressani@gmail.com>
2019-03-01 00:30:10 -0300

    Add one to many association between recipe and chef

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_154642) do
+ActiveRecord::Schema.define(version: 2019_03_01_030834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_02_26_154642) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "chef_id"
   end
 
   create_table "users", force: :cascade do |t|
--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -3,7 +3,13 @@ require 'test_helper'
 class RecipeTest < ActiveSupport::TestCase
 
   def setup
-    @recipe = Recipe.new(name: "vegetable", description: "greate vegetable recipe")
+    @chef = Chef.create!(chefname: "Bressani", email: "bressani@jair.junior")
+    @recipe = @chef.recipes.build(name: "vegetable", description: "greate vegetable recipe")
+  end
+
+  test "recipe withouth chef should be invalid" do
+    @recipe.chef_id = nil
+    assert_not @recipe.valid?
   end
 
   test "recipe should be valid" do
