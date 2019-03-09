bba650bd77714569ca48f1b2e0f872ef235b727b
bressani <jr.bressani@gmail.com>
2019-03-09 12:56:08 -0300

    Implement chef signup

---
+++ test/integration/chefs_signup_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class ChefsSignupTest < ActionDispatch::IntegrationTest
+  test "should get signup path" do
+    get signup_path
+    assert_response :success
+  end
+
+  test "reject an invalid signup" do
+    get signup_path
+    assert_no_difference 'Chef.count' do
+      post chefs_path, params: { chef: { chefname: " ", email: " ", password: "password",
+        password_confirmation: " " } }
+    end
+    assert_template 'chefs/new'
+  end
+
+  test "accept valid signup" do
+    get signup_path
+    assert_difference 'Chef.count', 1 do
+      post chefs_path, params: { chef: { chefname: "bressani", email: "bressani@jair.junior", password: "password", password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'chefs/show'
+    assert_match "Welcome bressani to MyRecipes App!", response.body
+    assert_match 'bressani', response.body
+    assert_match 'bressani@jair.junior', response.body
+  end
+end
