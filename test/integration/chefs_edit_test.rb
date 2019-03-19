c99527a8ba12b7c49458e609e3ec04b588df41d8
bressani <jr.bressani@gmail.com>
2019-03-19 01:12:27 -0300

    Restrict actions on users and recipes through authentication

--- test/integration/chefs_edit_test.rb
+++ test/integration/chefs_edit_test.rb
@@ -7,6 +7,7 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
   end
 
   test "reject an invalid edit" do
+    sign_in_as(@chef, "password")
     get edit_chef_path(@chef)
     assert_template 'chefs/edit'
     patch chef_path(@chef), params: { chef: { chefname: " ", email: "jair@email.com" } }
@@ -15,6 +16,7 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
   end
 
   test "accept valid signup" do
+    sign_in_as(@chef, "password")
     get edit_chef_path(@chef)
     assert_template 'chefs/edit'
     patch chef_path(@chef), params: { chef: { chefname: 'jair1', email: 'jair1@email.com' } }
